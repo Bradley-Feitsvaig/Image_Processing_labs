775b489dd3820f3af2e8202f1545874c75fa7c77
Bradley <bradley.feitsvaig@gmail.com>
2024-02-24 15:30:47 +0200

    lab3 q1 images 1-6

--- lab3/q1/ImageRecreation.py
+++ lab3/q1/ImageRecreation.py
@@ -11,7 +11,7 @@ def mse(im1_path, im2_path):
     return mse
 
 
-def recreate_image_with_kernel(input_image, kernel, output_image, border_type, border_value=128):
+def recreate_image_with_kernel(input_image, kernel, border_type, border_value=128):
     # Calculate the border width from the kernel size
     border_width = kernel.shape[0] // 2
 
@@ -33,34 +33,38 @@ def recreate_image_with_kernel(input_image, kernel, output_image, border_type, b
         # For other border types, apply the kernel using filter2D which handles borders internally
         recreated = cv2.filter2D(input_image, -1, kernel, borderType=border_type)
 
+    return recreated
+
+
+def save_image_and_calculate_mse(recreated, output_image):
     cv2.imwrite(f'recreated_images/{output_image}.jpg', recreated)
     mse(f'recreated_images/{output_image}.jpg', f'{output_image}.jpg')
 
 
-def recreate_image_mean_value_of_each_row(input_image, output_image_path):
+def recreate_image_mean_value_of_each_row(input_image):
     row_means = np.mean(input_image, axis=1)  # mean value of each row
 
     # Create an output image where each row has the mean value of the original row
     output_image = np.zeros_like(input_image)
     for i, mean in enumerate(row_means):
         output_image[i, :] = mean
-    cv2.imwrite(f'recreated_images/{output_image_path}.jpg', output_image)
-    mse(f'recreated_images/{output_image_path}.jpg', f'{output_image_path}.jpg')
+
+    return output_image
 
 
-def recreate_image_with_medianBlur(input_image, output_image_path):
+def recreate_image_with_medianBlur(input_image):
     median_filtered_image = cv2.medianBlur(input_image, 11)
-    cv2.imwrite(f'recreated_images/{output_image_path}.jpg', median_filtered_image)
-    mse(f'recreated_images/{output_image_path}.jpg', f'{output_image_path}.jpg')
+    return median_filtered_image
 
 
-def recreate_image_with_motionBlur(input_image, output_image_path, ksize):
+def recreate_image_with_motionBlur(input_image, ksize):
     pad_size = ksize // 6
     kernel_motion_blur = np.zeros((kernel_size, kernel_size))
     kernel_motion_blur[:, int((kernel_size - 1) / 2)] = np.ones(kernel_size)
     kernel_motion_blur = kernel_motion_blur / kernel_size
     # # Apply padding with a constant value
-    padded_image = cv2.copyMakeBorder(input_image, pad_size, pad_size, pad_size, pad_size, cv2.BORDER_CONSTANT, value=(0,))
+    padded_image = cv2.copyMakeBorder(input_image, pad_size, pad_size, pad_size, pad_size, cv2.BORDER_CONSTANT,
+                                      value=(0,))
 
     # Apply Gaussian blur to the padded image
     recreated = cv2.filter2D(padded_image, -1, kernel_motion_blur)
@@ -68,46 +72,51 @@ def recreate_image_with_motionBlur(input_image, output_image_path, ksize):
     # # Correctly crop the image back to original size
     blurred_image = recreated[pad_size:-pad_size, pad_size:-pad_size]
 
-    cv2.imwrite(f'recreated_images/{output_image_path}.jpg', blurred_image)
-    mse(f'recreated_images/{output_image_path}.jpg', f'{output_image_path}.jpg')
+    return blurred_image
 
 
-def recreate_image_with_bilateralFilter(input_image, output_image):
+def recreate_image_with_bilateralFilter(input_image):
     recreated = cv2.bilateralFilter(input_image, d=9, sigmaColor=75, sigmaSpace=75)
-    cv2.imwrite(f'recreated_images/{output_image}.jpg', recreated)
-    mse(f'recreated_images/{output_image}.jpg', f'{output_image}.jpg')
+    return recreated
 
 
 # Load the image in grayscale
 image = cv2.imread('1.jpg', cv2.IMREAD_GRAYSCALE)
 
 # image 1
-recreate_image_mean_value_of_each_row(image, 'image_1')
+recreated = recreate_image_mean_value_of_each_row(image)
+save_image_and_calculate_mse(recreated, 'image_1')
 
 # image 2
 kernel_size = 11
 kernel = np.ones((kernel_size, kernel_size)) / (kernel_size ** 2)
-recreate_image_with_kernel(image, kernel, 'image_2', border_type=cv2.BORDER_REFLECT)
+recreated = recreate_image_with_kernel(image, kernel, border_type=cv2.BORDER_REFLECT)
+save_image_and_calculate_mse(recreated, 'image_2')
 
 # image 3
-recreate_image_with_medianBlur(image, 'image_3')
+recreated = recreate_image_with_medianBlur(image)
+save_image_and_calculate_mse(recreated, 'image_3')
 
 # image 4
-# recreate_image_with_motionBlur(image,'image_4', 14)
-
-# recreate_image_with_GaussianBlur(image, 'image_4')
-# recreate_image_with_GaussianBlur(image, 'image_4', 7, 9)
 kernel_size = 15
 kernel_motion_blur = np.zeros((kernel_size, kernel_size))
 kernel_motion_blur[:, int((kernel_size - 1) / 2)] = np.ones(kernel_size)
 kernel_motion_blur = kernel_motion_blur / kernel_size
-recreate_image_with_kernel(image, kernel_motion_blur, 'image_4', border_type=cv2.BORDER_CONSTANT)
+recreated = recreate_image_with_kernel(image, kernel_motion_blur, border_type=cv2.BORDER_CONSTANT)
+save_image_and_calculate_mse(recreated, 'image_4')
+
+# image 5
+b_g = cv2.GaussianBlur(image, (21, 21), 0, borderType=cv2.BORDER_WRAP)
+B_sharp = image - b_g
+B_sharp += 128
+save_image_and_calculate_mse(B_sharp, 'image_5')
 
 # image 6
 kernel = np.array([[-1],
                    [0],
                    [1]])
-recreate_image_with_kernel(image, kernel, 'image_6', border_type=cv2.BORDER_DEFAULT)
+recreated = recreate_image_with_kernel(image, kernel, border_type=cv2.BORDER_DEFAULT)
+save_image_and_calculate_mse(recreated, 'image_6')
 
 # # image 7
 # # Image dimensions
